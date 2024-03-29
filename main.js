4b26a57b1ba56164531e4946c182a33bbcefca78
Misbah Shakil <141862743+misbahshakil233@users.noreply.github.com>
2024-03-29 12:20:58 +0500

    while loop
    I want to my program again run without tsc and  node command, so I apply while loop

---
+++ main.js
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+async function main() {
+    //
+    let againStart = true;
+    while (againStart) {
+        let myBalance = 10000;
+        let myPin = 1234;
+        let pinAnswer = await inquirer.prompt([
+            {
+                name: "pin",
+                message: "Enter your Pin Number",
+                type: "number",
+            }
+        ]);
+        if (pinAnswer.pin === myPin) {
+            console.log("Correct pin code!!");
+            let operationAns = await inquirer.prompt([
+                {
+                    name: "operation",
+                    message: "Please select an option?",
+                    type: "list",
+                    choices: ["Withdraw", "Check Balance"] // Corrected the array structure
+                }
+            ]);
+            if (operationAns.operation === "Withdraw") {
+                let amountAns = await inquirer.prompt([
+                    {
+                        name: "amount",
+                        message: "Enter Amount",
+                        type: "number",
+                    }
+                ]);
+                //
+                if (amountAns.amount <= myBalance) {
+                    myBalance -= amountAns.amount;
+                    console.log(`your remaining balance is : ${myBalance}`);
+                }
+                else if (amountAns.amount >= myBalance) {
+                    console.log("please enter correct amount");
+                }
+            }
+            else if (operationAns.operation === "Check Balance") {
+                console.log(`your current balance is : ${myBalance}`);
+            }
+        }
+        else {
+            console.log("Incorrect pin code!!");
+        }
+        const again = await inquirer.prompt({
+            name: "restart",
+            message: "Do you Want to Again ?",
+            type: "input"
+        });
+        againStart = (again.restart.toLowerCase() === 'y');
+    }
+}
+main();
